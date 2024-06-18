1742ef3993048d224abf96a0000a699b0df31e94
finnrck <reineckefinn@web.de>
2024-06-18 21:26:51 +0200

    fetchData#1

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const Result = () => {
     const location = useLocation();
-
-    // Anbindung backend für daten
+    const [data, setData] = useState(null);
     
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("" + location.state.champion); //datenbank endpunkt einfügen
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Ergebnisse</h1>
             <p>Champion: {location.state.champion}</p>
+            <p>Backend-Daten: {JSON.stringify(data)}</p>
         </div>
     );
 }
