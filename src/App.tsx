f499afa54ab87f80d796f45916fe488f3d559be5
finnrck <reineckefinn@web.de>
2024-06-19 22:31:00 +0200

    revert to working commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import InputTest from './components/InputTest';
 import Result from './components/Result';
-import { checkData } from './utils/checkData';
 import './App.css';
 
 const App: React.FC = () => {
 
-  useEffect(() => {
-    checkData();
-  }, []);
 
   return (
     <Router>
